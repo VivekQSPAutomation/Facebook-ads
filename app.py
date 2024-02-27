750552b41b0dfd8f410b967060df071996a69213
Vivekquotient <136687071+Vivekquotient@users.noreply.github.com>
2024-02-27 09:07:47 +0530

    Kubernetes zip code

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
 import csv
 import io
+from math import radians
 
+import numpy as np
 from fastapi import FastAPI, File, UploadFile, Form
 from fastapi.requests import Request
 from fastapi.responses import FileResponse
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from pandas import pandas as pd
-from sklearn.metrics.pairwise import haversine_distances
-from math import radians
-import numpy as np
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
-
 df = pd.read_csv('zip_updated.csv')
-df.dropna(subset=['lat', 'lon'], inplace=True)  # Drop rows with missing values
+df.dropna(subset=['lat', 'lon'], inplace=True)
+
+
+def format_postcode(code):
+    return f'{int(code):05d}'
 
 
 def filter_postcodes(lat, long, distance_threshold):
@@ -25,10 +27,10 @@ def filter_postcodes(lat, long, distance_threshold):
     lat2, lon2 = np.radians(df['lat'].values), np.radians(df['lon'].values)
     dlon = lon2 - long
     dlat = lat2 - lat
-    a = np.sin(dlat/2)**2 + np.cos(lat) * np.cos(lat2) * np.sin(dlon/2)**2
-    c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1-a))
-    dist = 3959 * c  # Radius of Earth in miles
-    postcodes = df.loc[dist <= distance_threshold, 'code'].tolist()
+    a = np.sin(dlat / 2) ** 2 + np.cos(lat) * np.cos(lat2) * np.sin(dlon / 2) ** 2
+    c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1 - a))
+    dist = 3959 * c
+    postcodes = df.loc[dist <= distance_threshold, 'code'].apply(format_postcode).tolist()
     return postcodes
 
 
@@ -48,7 +50,10 @@ async def upload_csv(request: Request, file: UploadFile = File(...), radius: int
     common_zips_data = df[df['code'].isin(common_zips)]
     common_zips_lat_long = common_zips_data[['lat', 'lon', 'code']]
     json_string = common_zips_lat_long.values.tolist()
-    csv_file = "output.csv"
+
+    # filename, ext = os.path.splitext(file.filename)
+    # cleaned_filename = ''.join(c if c.isalnum() or c in ['.', '_'] else '_' for c in filename)
+    csv_file = f"output.csv"
     with open(csv_file, 'w', newline='') as csvfile:
         fieldnames = ['ZipCode', 'Extended Zipcodes']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
@@ -56,8 +61,8 @@ async def upload_csv(request: Request, file: UploadFile = File(...), radius: int
         for lat, long, code in json_string:
 
             extended_zip_codes = filter_postcodes(lat, long, radius_km)
-            print(extended_zip_codes)
-            writer.writerow({'ZipCode': code, 'Extended Zipcodes': extended_zip_codes})
+            extend = ",".join(extended_zip_codes)
+            writer.writerow({'ZipCode': format_postcode(int(code)), 'Extended Zipcodes': extend})
 
     download_url = f"/download/{csv_file}"
     return templates.TemplateResponse("index.html", {"request": request, "file_name": csv_file,
